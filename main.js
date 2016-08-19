9f379eb935b15fae50d7ccd06b7322e6bd200376
Juan Edi <jedi@manas.com.ar>
2016-08-19 14:37:55 -0300

    Fixes

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+#! env node
 var d3 = require("d3")
 
 var points = {
@@ -6,18 +7,23 @@ var points = {
     50:  { name: "lt50",   expression: "[pixel] < 50"   },
     100: { name: "lt100",  expression: "[pixel] < 100"  },
     150: { name: "lt150",  expression: "[pixel] < 150"  },
-    255: { name: "geq200", expression: "[pixel] >= 150" },
+    255: { name: "geq150", expression: "[pixel] >= 150" },
 };
 
+var domain = [5,255];
 var colorRange = ["#ffc800","#ff0000"];
 
-function mapserverClass(name, expression, color) {
+function parseColor(color) {
     var match = /rgb\((.*), (.*), (.*)\)/.exec(color);
+    return [match[1], match[2], match[3]];
+}
+
+function mapserverClass(name, expression, color) {
     return `CLASS
   NAME ${name}
   EXPRESSION (${expression})
   STYLE
-   COLOR ${match[1]} ${match[2]} ${match[3]}
+   COLOR ${color[0]} ${color[1]} ${color[2]}
   END
 END
 `;
@@ -26,16 +32,29 @@ END
 var scales = {
     linear: d3.scaleLinear(),
     pow2: d3.scalePow().exponent(2),
-    log: d3.scalePow().exponent(0.5),
+    sqrt: d3.scalePow().exponent(0.5),
+    log: d3.scaleLog(),
 }
 
-var args = process.argv.slice(2)
-var scaleName = args[0]
-var scaleInstance = scales[scaleName].domain([0, 255]).range(colorRange).nice();
+var args = process.argv.slice(2);
+var output = args[0];
+var scaleName = args[1];
+
+var scaleInstance = scales[scaleName].domain(domain).nice();
 
 for (var point in points) {
     var config = points[point];
-    var color = scaleInstance(point);
-    var classDef = mapserverClass(config.name, config.expression, color);
-    console.log(classDef);
+
+    if (output == "values") {
+        scaleInstance = scaleInstance.range([0, 1000]);
+        console.log(scaleInstance(point));
+    } else {
+        scaleInstance = scaleInstance.range(colorRange);
+        var color = parseColor(scaleInstance(point));
+        if (output == "color") {
+            console.log(color);
+        } else if (output == "config") {
+            console.log(mapserverClass(config.name, config.expression, color));
+        }
+    }
 }
